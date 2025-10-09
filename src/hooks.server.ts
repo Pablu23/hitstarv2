5dda281985331be698cc49bdfa08b9bfe6722a24
Pablu23 <pablu23@gmx.de>
2025-10-09 19:46:56 +0200

    WIP: Make login with api work

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,40 +1,43 @@
-import { db } from '$lib/server/db';
-import { sessionsTable } from '$lib/server/db/schema';
-import { eq } from 'drizzle-orm';
-import { redirect, type Handle } from '@sveltejs/kit';
+import { redirect, type Handle, type HandleFetch } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const sessionId = event.cookies.get('session_id');
-  let user = {
-    isLoggedIn: false,
-    email: "",
-    username: ""
-  };
+	const sessionId = event.cookies.get('session_id');
+	let user = {
+		isLoggedIn: false,
+		email: '',
+		username: ''
+	};
 
-  if (sessionId) {
-    const session = await db.query.sessionsTable.findFirst({
-      with: {
-        user: true
-      },
-      where: eq(sessionsTable.id, sessionId)
-    });
+	if (sessionId) {
+		const response = await fetch('http://localhost/api/user/me', {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		console.log(response.status);
+		console.log(JSON.stringify(response));
 
-    if (session && session.user && session.user.email) {
-      user = {
-        isLoggedIn: true,
-        email: session.user.email,
-        username: session.user.username || "Unknown username"
-      };
-    }
-  }
+		if (response.status >= 200 && response.status < 300) {
+			const uBody = await response.json();
+			user = {
+				isLoggedIn: true,
+				email: uBody.email,
+				username: uBody.display_name || 'Unknown username'
+			};
+		}
+	}
 
-  if (event.url.pathname.startsWith("/private") && !user.isLoggedIn) {
-    redirect(307, "/error");
-  } else if (event.url.pathname.startsWith("/api") && !user.isLoggedIn) {
-    return new Response(null, { status: 401 });
-  }
+	if (event.url.pathname.startsWith('/private') && !user.isLoggedIn) {
+		redirect(307, '/error');
+	} else if (event.url.pathname.startsWith('/api') && !user.isLoggedIn) {
+		return new Response(null, { status: 401 });
+	}
 
-  event.locals.user = user;
-  const response = await resolve(event);
-  return response;
-}
+	event.locals.user = user;
+	const response = await resolve(event);
+	return response;
+};
+
+// export const handleFetch: HandleFetch = async({request, fetch}) => {
+//   if (request.url
+// }
